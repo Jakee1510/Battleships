91d29153fa17e95e49f51f1957adda5f377349be
alidawoud <101697115@student.swin.edu.au>
2018-04-29 14:57:40 +1000

    Fixed high scores not saving

--- CS_Battleships/src/HighScoreController.cs
+++ CS_Battleships/src/HighScoreController.cs
@@ -211,7 +211,7 @@ sealed class HighScoreController
             _Scores.RemoveAt(_Scores.Count - 1);
             _Scores.Add(s);
             _Scores.Sort();
-            
+            SaveScores();
             GameController.EndCurrentState();
         }
     }
